20df7db7305a8b51273c180ad18d2e4d6f81d5d9
Alok Maurya <alok.maurya@w3villa.com>
2023-11-20 13:20:24 +0530

    video player image view added

--- lib/widgets/video_player_widget.dart
+++ lib/widgets/video_player_widget.dart
@@ -12,12 +12,17 @@ class VideoPlayerWidget extends StatefulWidget {
 class _VideoPlayerWidgetState extends State<VideoPlayerWidget> {
   VideoPlayerController? controller;
   bool isPlaying = false;
+  bool isInitialized = false;
 
   @override
   void initState() {
     super.initState();
     controller = VideoPlayerController.networkUrl(Uri.parse(widget.playerUrl));
-    controller!.initialize().then((value) {});
+    controller!.initialize().then((value) {
+      setState(() {
+        isInitialized = true;
+      });
+    });
   }
 
   @override
@@ -35,61 +40,65 @@ class _VideoPlayerWidgetState extends State<VideoPlayerWidget> {
           return Future.value(true);
         }
       },
-      child: Stack(
-        children: [
-          Dialog.fullscreen(
-            child: Center(
-                child: controller!.value.isInitialized
-                    ? VideoPlayer(controller!)
-                    : VideoPlayer(controller!)),
-          ),
-          Align(
-            alignment: Alignment.topLeft,
-            child: IconButton(
-                onPressed: () {
-                  if (isPlaying) {
-                    controller!.pause();
-                    setState(() {
-                      isPlaying = !isPlaying;
-                    });
-                  }
-                  Navigator.pop(context);
-                },
-                icon: const Icon(
-                  Icons.arrow_back,
-                  color: Colors.white,
-                  size: 28,
-                )),
-          ),
-          Align(
-            alignment: Alignment.center,
-            child: SizedBox(
-              width: 40,
-              height: 40,
-              child: IconButton(
-                onPressed: () {
-                  if (isPlaying) {
-                    controller!.pause();
-                    setState(() {
-                      isPlaying = !isPlaying;
-                    });
-                  } else {
-                    controller!.play();
-                    setState(() {
-                      isPlaying = !isPlaying;
-                    });
-                  }
-                },
-                icon: Icon(
-                  isPlaying ? Icons.pause : Icons.play_circle,
-                  color: Colors.white,
-                  size: 48,
+      child: isInitialized
+          ? Stack(
+              children: [
+                Dialog.fullscreen(
+                  child: Center(
+                      child: controller!.value.isInitialized
+                          ? VideoPlayer(controller!)
+                          : VideoPlayer(controller!)),
                 ),
-              ),
+                Align(
+                  alignment: Alignment.topLeft,
+                  child: IconButton(
+                      onPressed: () {
+                        if (isPlaying) {
+                          controller!.pause();
+                          setState(() {
+                            isPlaying = !isPlaying;
+                          });
+                        }
+                        Navigator.pop(context);
+                      },
+                      icon: const Icon(
+                        Icons.arrow_back,
+                        color: Colors.white,
+                        size: 28,
+                      )),
+                ),
+                Align(
+                  alignment: Alignment.center,
+                  child: SizedBox(
+                    width: 40,
+                    height: 40,
+                    child: IconButton(
+                      onPressed: () {
+                        if (isPlaying) {
+                          controller!.pause();
+                          setState(() {
+                            isPlaying = !isPlaying;
+                          });
+                        } else {
+                          controller!.play();
+                          setState(() {
+                            isPlaying = !isPlaying;
+                          });
+                        }
+                      },
+                      icon: Icon(
+                        isPlaying ? Icons.pause : Icons.play_circle,
+                        color: Colors.white,
+                        size: 48,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            )
+          : const Center(
+              child: CircularProgressIndicator.adaptive(),
             ),
-          ),
-        ],
-      ),
     );
   }
 }
